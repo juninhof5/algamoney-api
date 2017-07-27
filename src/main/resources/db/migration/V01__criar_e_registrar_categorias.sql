903c3b8cbca2975aa6b8d85dda7b605133b7bc62
juninho <juninho@192.168.0.13>
2017-07-26 21:37:20 -0300

    Primeiro Commit

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categoria (
+	codigo BIGINT(20)  PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(50) NOT NULL
+
+) ENGINE=InnoDB default CHARSET=utf8;
+
+insert into categoria (nome) values ('Lazer');
+insert into categoria (nome) values ('Alimentação');
+insert into categoria (nome) values ('Supermercado');
+insert into categoria (nome) values ('Farmácia');
+insert into categoria (nome) values ('Outros');
