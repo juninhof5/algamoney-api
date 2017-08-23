dd74f124856aee09e813f3caee3cde81d7f463e3
juninho <juninho@192.168.0.13>
2017-08-23 07:31:42 -0300

    5.5. Validando lançamento com Bean Validation

--- src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -13,33 +13,41 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
 public class Lancamento {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotNull
 	private String descricao;
 
+	@NotNull
 	@Column(name = "data_vencimento")
 	private LocalDate dataVencimento;
 
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
+
 	private String observacao;
-	
+
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
-	
+
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
-	
+
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
@@ -115,7 +123,7 @@ public class Lancamento {
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
